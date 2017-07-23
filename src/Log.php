cd93500cd71b7952d7240438f89f6e36d3d84ce4
Chris Ullyott <chris@monkdevelopment.com>
2017-07-23 09:34:10 -0700

    Comments

--- src/Log.php
+++ src/Log.php
@@ -1,25 +1,46 @@
 <?php
 
 /**
- * Logs data in JSON.
+ * Persists data in a JSON file.
  *
  * @author Chris Ullyott <chris@monkdevelopment.com>
  */
 class Log
 {
+    /**
+     * A path for the JSON file.
+     *
+     * @var string
+     */
     private $file;
 
+    /**
+     * Constructor.
+     *
+     * @param string $file A path for the JSON file
+     */
     public function __construct($file)
     {
         $this->file = $file;
         File::createDir(dirname($this->file));
     }
 
+    /**
+     * Get the path for the JSON file.
+     *
+     * @return string
+     */
     public function getFile()
     {
         return $this->file;
     }
 
+    /**
+     * Get a stored value by key.
+     *
+     * @param  string|integer $key The item key
+     * @return mixed
+     */
     public function get($key)
     {
         $array = $this->getAll();
@@ -27,6 +48,11 @@ class Log
         return isset($array[$key]) ? $array[$key] : null;
     }
 
+    /**
+     * Get all stored data.
+     *
+     * @return array
+     */
     public function getAll()
     {
         $json = File::read($this->getFile());
@@ -35,6 +61,14 @@ class Log
         return is_array($array) ? $array : array();
     }
 
+    /**
+     * Persist a value by key.
+     *
+     * @param string|integer $key   The key to use
+     * @param string|integer $value The value to store
+     * @return self
+     */
+    public function set($key, $value = null)
     {
         $array = $this->getAll();
 
@@ -56,6 +90,12 @@ class Log
         return $this;
     }
 
+    /**
+     * Merge an array into the existing dataset.
+     *
+     * @param  array $array The array of data to store
+     * @return self
+     */
     public function merge(array $array)
     {
        $array = array_merge($this->getAll(), $array);
@@ -65,6 +105,12 @@ class Log
        return $this;
     }
 
+    /**
+     * Delete an item by key.
+     *
+     * @param string|integer $key The key to use
+     * @return self
+     */
     public function delete($key)
     {
         $array = $this->getAll();
@@ -78,6 +124,12 @@ class Log
         return $this;
     }
 
+    /**
+     * Persist an array of data.
+     *
+     * @param  array $array The array of data to store
+     * @return bool         Whether the file was written
+     */
     private function save(array $array)
     {
         return File::write($this->getFile(), json_encode($array));
